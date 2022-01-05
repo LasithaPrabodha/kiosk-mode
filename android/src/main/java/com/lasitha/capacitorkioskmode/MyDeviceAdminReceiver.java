d11722709ff44424ea50c0298440834f7afb90ee
Lasitha Prabodha <lasitha.weligampola@cenium.com>
2022-01-05 14:57:37 +0530

    manifest update

--- android/src/main/java/com/lasitha/capacitorkioskmode/MyDeviceAdminReceiver.java
+++ android/src/main/java/com/lasitha/capacitorkioskmode/MyDeviceAdminReceiver.java
@@ -6,35 +6,13 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
-
 import androidx.annotation.NonNull;
 
 public class MyDeviceAdminReceiver extends DeviceAdminReceiver {
+
     private String TAG = MyDeviceAdminReceiver.class.getSimpleName();
 
     public static ComponentName getComponentName(Context context) {
         return new ComponentName(context.getApplicationContext(), MyDeviceAdminReceiver.class);
-    }
-
-    @SuppressLint("UnsafeProtectedBroadcastReceiver")
-    @Override
-    public void onReceive(@NonNull Context context, @NonNull Intent intent) {
-        Intent newIntent = new Intent(context, KioskMode.class);
-
-        newIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-
-        context.startActivity(newIntent);
-    }
-
-    @Override
-    public void onLockTaskModeEntering(@NonNull Context context, @NonNull Intent intent, @NonNull String pkg) {
-        super.onLockTaskModeEntering(context, intent, pkg);
-        Log.d(TAG, "onLockTaskModeEntering");
-    }
-
-    @Override
-    public void onLockTaskModeExiting(@NonNull Context context, @NonNull Intent intent) {
-        super.onLockTaskModeExiting(context, intent);
-        Log.d(TAG, "onLockTaskModeExiting");
     }
 }
